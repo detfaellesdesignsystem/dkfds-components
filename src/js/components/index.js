97f3932118b210925e190df763fc4a8c5f930e0b
Emil Jensen <eje@netcompany.com>
2018-12-08 16:24:25 +0100

    removed vaidator.js

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -2,7 +2,6 @@ module.exports = {
   accordion:  require('./accordion'),
   navigation: require('./navigation'),
   skipnav:    require('./skipnav'),
-  validator:  require('./validator'),
   regexmask:  require('./regex-input-mask'),
   collapse:   require('./collapse')
 };
