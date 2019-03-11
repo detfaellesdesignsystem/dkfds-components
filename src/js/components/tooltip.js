5a9e058dcf162f818fd666b32d8c3da47d528057
Kristina Jensen <krj@netcompany.com>
2019-03-11 08:35:03 +0100

    Removed code which results in error when loading js file

--- src/js/components/tooltip.js
+++ src/js/components/tooltip.js
@@ -2,24 +2,19 @@
 const domready = require('domready');
 const select = require('../utils/select');
 //Import tippy.js (https://atomiks.github.io/tippyjs/)
-const tippy = require("../../vendor/tippyjs/tippy.js");
+const tippy = require('../../vendor/tippyjs/tippy.js');
 
-var initTippy = function(){
+var initTippy = function (){
     //init tooltip on elements with the .js-tooltip class
     tippy('.js-tooltip', { 
         duration: 0,
         arrow: true
-    }) 
-}
+    });
+};
 
 domready(() => {
     initTippy();
 });
-
-var body = select('body')[0];
-body.addEventListener('init-tooltips', function (e) {
-    initTippy();
-}, false);
 
 
 
