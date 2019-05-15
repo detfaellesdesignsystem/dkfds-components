6a3d22652c223050944aaae71f5161a65b1d87a9
Kristina Jensen <krj@netcompany.com>
2019-05-15 10:33:47 +0200

    Update regex

--- src/js/components/regex-input-mask.js
+++ src/js/components/regex-input-mask.js
@@ -16,45 +16,50 @@ const modifierState = {
 };
 
 function inputRegexMask(event) {
-
-    if(modifierState.ctrl || modifierState.command) {
-        return;
+  if(modifierState.ctrl || modifierState.command) {
+    return;
+  }
+  var newChar = null;
+  if(typeof event.key !== "undefined"){
+    if(event.key.length === 1){
+      newChar = event.key;
     }
-    var newChar = null;
-    if(typeof event.key !== "undefined"){
-        if(event.key.length === 1){
-            newChar = event.key;
-        }
+  } else {
+    if(!event.charCode){
+      newChar = String.fromCharCode(event.keyCode);
     } else {
-        if(!event.charCode){
-            newChar = String.fromCharCode(event.keyCode);
-        } else {
-            newChar = String.fromCharCode(event.charCode);
-        }
+      newChar = String.fromCharCode(event.charCode);
     }
+  }
+
+  var regexStr = this.getAttribute('data-input-regex');
+
+  if(event.type !== undefined && event.type === 'paste'){
+    console.log('paste');
+  } else{
     var element = null;
     if(event.target !== undefined){
-        element = event.target;
+      element = event.target;
     }
     if(newChar !== null && element !== null) {
-        if(newChar.length > 0){
-            if(element.type === "number"){
-                var newValue = this.value;//Note input[type=number] does not have .selectionStart/End (Chrome).
-            }else{
-                var newValue = this.value.slice(0, element.selectionStart) + this.value.slice(element.selectionEnd) + newChar; //removes the numbers selected by the user, then adds new char.
-            }
-
-            var regexStr = this.getAttribute("data-input-regex");
-            var r = new RegExp(regexStr);
-            if(r.exec(newValue) === null){
-                if (event.preventDefault) {
-                  event.preventDefault();
-                } else {
-                  event.returnValue = false;
-                }
-            }
+      if(newChar.length > 0){
+        let newValue = this.value;
+        if(element.type === 'number'){
+          newValue = this.value;//Note input[type=number] does not have .selectionStart/End (Chrome).
+        }else{
+          newValue = this.value.slice(0, element.selectionStart) + this.value.slice(element.selectionEnd) + newChar; //removes the numbers selected by the user, then adds new char.
         }
-    }
+
+        var r = new RegExp(regexStr);
+        if(r.exec(newValue) === null){
+          if (event.preventDefault) {
+            event.preventDefault();
+          } else {
+            event.returnValue = false;
+          }
+        }
+      }
+    }}
 }
 
 module.exports = behavior({
