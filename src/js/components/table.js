9c1b14970bd8a7f790a6015815ff2860f9b2e2ab
Kristina Jensen <krj@netcompany.com>
2019-07-02 11:04:24 +0200

    semicolon

--- src/js/components/table.js
+++ src/js/components/table.js
@@ -3,7 +3,7 @@ const domready = require('domready');
 const forEach = require('array-foreach');
 
 domready(() => {
-	new ResponsiveTables(); 
+	new ResponsiveTables();
 });
 export default class ResponsiveTables {
     constructor() {
@@ -20,11 +20,11 @@ export default class ResponsiveTables {
         const headerCellEls =  select('thead th, thead td', tableEl);
 
         //const headerCellEls = select(el.querySelectorAll('thead th, thead td');
-    
+
         if (headerCellEls.length) {
             const bodyRowEls = select('tbody tr', tableEl);
             Array.from(bodyRowEls).forEach(rowEl => {
-                let cellEls = rowEl.children
+                let cellEls = rowEl.children;
                 if (cellEls.length === headerCellEls.length) {
                     Array.from(headerCellEls).forEach((headerCellEl, i) => {
                         // Grab header cell text and use it body cell data title.
