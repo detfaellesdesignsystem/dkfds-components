8e69bb5a59007fbd9bb8a7f8ecbd29c6746f659f
Kristina Jensen <krj@netcompany.com>
2019-09-02 15:49:56 +0200

    Fix table error

--- src/js/components/table.js
+++ src/js/components/table.js
@@ -1,33 +1,35 @@
 const select = require('../utils/select');
 
 class ResponsiveTable {
-    constructor(table) {
+    constructor (table) {
         this.insertHeaderAsAttributes(table);
     }
 
     // Add data attributes needed for responsive mode.
-    insertHeaderAsAttributes(tableEl){
-        if (!tableEl) return
+    insertHeaderAsAttributes (tableEl){
+        if (!tableEl) return;
 
-        let headerCellEls =  tableEl.getElementsByTagName('thead')[0].getElementsByTagName('th');
-        if(headerCellEls.length == 0) {
-          headerCellEls = tableEl.getElementsByTagName('thead')[0].getElementsByTagName('td');
-        }
+        let header =  tableEl.getElementsByTagName('thead');
+        if(header.length !== 0) {
+          let headerCellEls = header[ 0 ].getElementsByTagName('th');
+          if (headerCellEls.length == 0) {
+            headerCellEls = header[ 0 ].getElementsByTagName('td');
+          }
 
-        if (headerCellEls.length) {
+          if (headerCellEls.length) {
             const bodyRowEls = select('tbody tr', tableEl);
             Array.from(bodyRowEls).forEach(rowEl => {
-                let cellEls = rowEl.children
-                if (cellEls.length === headerCellEls.length) {
-                    Array.from(headerCellEls).forEach((headerCellEl, i) => {
-                        // Grab header cell text and use it body cell data title.
-                        cellEls[i].setAttribute('data-title', headerCellEl.textContent)
-                    });
-                }
+              let cellEls = rowEl.children;
+              if (cellEls.length === headerCellEls.length) {
+                Array.from(headerCellEls).forEach((headerCellEl, i) => {
+                  // Grab header cell text and use it body cell data title.
+                  cellEls[ i ].setAttribute('data-title', headerCellEl.textContent);
+                });
+              }
             });
+          }
         }
     }
 }
-
 
 module.exports = ResponsiveTable;
