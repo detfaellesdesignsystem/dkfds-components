d42413e59f09f1757c49fe5622fdfacc4f01276b
Helle Juhl <hef@netcompany.com>
2022-09-19 09:56:58 +0200

    Updated back-to-top button rules to ignore open step guides

--- src/js/components/back-to-top.js
+++ src/js/components/back-to-top.js
@@ -12,30 +12,50 @@ BackToTop.prototype.init = function(){
     window.addEventListener('scroll', function(e) {
         updateBackToTopButton(backtotopbutton);
     });
+
+    window.addEventListener('resize', function(e) {
+        updateBackToTopButton(backtotopbutton);
+    });
 }
 
 function updateBackToTopButton(button) {
     let lastKnownScrollPosition = window.scrollY;
     let lastKnownWindowHeight = window.innerHeight;
 
-    let sidenav = document.querySelector('.sidenav-list');
+    let sidenavPresent = false;
+    let limit = lastKnownWindowHeight * 2; // Defines how far the user must scroll before the back-to-top-button should become visible
+    let sidenav = document.querySelector('.sidenav-list'); // Finds side navigations or step guides
 
+    // Check whether there's a sidenav present and ensure it's not hidden (offsetParent !== null)
     if (sidenav && sidenav.offsetParent !== null) {
-        let rect = sidenav.getBoundingClientRect();
-        if (rect.bottom < 0 && button.classList.contains('d-none')) {
-            button.classList.remove('d-none');
-            let eventShow = new Event('fds.backtotop.displayed');
-            button.dispatchEvent(eventShow);
+        sidenavPresent = true;
+
+        // Ignore the sidenav if it's caused by clicking an overflow-menu button
+        if (sidenav.closest(".overflow-menu-inner")?.previousElementSibling?.getAttribute('aria-expanded') === "true" &&
+            sidenav.closest(".overflow-menu-inner")?.previousElementSibling?.offsetParent !== null) {
+            sidenavPresent = false;
         }
-        else if (rect.bottom >= 0 && !button.classList.contains('d-none')) {
-            button.classList.add('d-none');
-            let eventHide = new Event('fds.backtotop.hidden');
-            button.dispatchEvent(eventHide);
+        // If there's a sidenav in the left side of the screen, calculate whether it is visible
+        else {
+            let rect = sidenav.getBoundingClientRect();
+            if (rect.bottom < 0 && button.classList.contains('d-none')) {
+                button.classList.remove('d-none');
+                let eventShow = new Event('fds.backtotop.displayed');
+                button.dispatchEvent(eventShow);
+            }
+            else if (rect.bottom >= 0 && !button.classList.contains('d-none')) {
+                button.classList.add('d-none');
+                let eventHide = new Event('fds.backtotop.hidden');
+                button.dispatchEvent(eventHide);
+            }
         }
     }
     else {
-        let limit = lastKnownWindowHeight * 2; // Defines how far the user must scroll before the button becomes visible
-        
+        sidenavPresent = false;
+    }
+
+    // If there's no sidenav, use the limit to decide whether to show or hide the back-to-top button
+    if (!sidenavPresent) {
         if (lastKnownScrollPosition >= limit && button.classList.contains('d-none')) {
             button.classList.remove('d-none');
             let eventShow = new Event('fds.backtotop.displayed');
@@ -47,7 +67,6 @@ function updateBackToTopButton(button) {
             button.dispatchEvent(eventHide);
         }
     }
-    
 }
 
 export default BackToTop;
