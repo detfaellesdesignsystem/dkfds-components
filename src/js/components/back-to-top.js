bceb0bb428cb773b3f7e8ec3a2695f06d06c8cf6
Helle Juhl <hef@netcompany.com>
2022-09-20 14:52:44 +0200

    Updated the back-to-top button to stay above the footer

--- src/js/components/back-to-top.js
+++ src/js/components/back-to-top.js
@@ -19,53 +19,90 @@ BackToTop.prototype.init = function(){
 }
 
 function updateBackToTopButton(button) {
-    let lastKnownScrollPosition = window.scrollY;
-    let lastKnownWindowHeight = window.innerHeight;
+    let docBody = document.body;
+    let docElem = document.documentElement;
+    let heightOfViewport = Math.max(docElem.clientHeight || 0, window.innerHeight || 0);
+    let heightOfPage = Math.max(docBody.scrollHeight, docBody.offsetHeight, docBody.getBoundingClientRect().height, 
+                                  docElem.scrollHeight, docElem.offsetHeight, docElem.getBoundingClientRect().height, docElem.clientHeight);
+    
+    let limit = heightOfViewport * 2; // The threshold selected to determine whether a back-to-top-button should be displayed
 
-    let sidenavPresent = false;
-    let limit = lastKnownWindowHeight * 2; // Defines how far the user must scroll before the back-to-top-button should become visible
-    let sidenav = document.querySelector('.sidenav-list'); // Finds side navigations or step guides
-
-    // Check whether there's a sidenav present and ensure it's not hidden (offsetParent !== null)
-    if (sidenav && sidenav.offsetParent !== null) {
-        sidenavPresent = true;
-
-        // Ignore the sidenav if it's caused by clicking an overflow-menu button
-        if (sidenav.closest(".overflow-menu-inner")?.previousElementSibling?.getAttribute('aria-expanded') === "true" &&
-            sidenav.closest(".overflow-menu-inner")?.previousElementSibling?.offsetParent !== null) {
-            sidenavPresent = false;
+    // If the page is too short, never show the button
+    if (limit > heightOfPage) {
+        if (!button.classList.contains('d-none')) {
+            button.classList.add('d-none');
         }
-        // If there's a sidenav in the left side of the screen, calculate whether it is visible
+    }
+    // If the page is long, calculate when to show the button
+    else {
+        if (button.classList.contains('d-none')) {
+            button.classList.remove('d-none');
+        }
+
+        let lastKnownScrollPosition = window.scrollY;
+        let footer = document.getElementsByTagName("footer")[0]; // If there are several footers, the code only applies to the first footer
+
+        // Show the button, if the user has scrolled too far down
+        if (lastKnownScrollPosition >= limit) {
+            if (!isFooterVisible(footer) && button.classList.contains('footer-sticky')) {
+                button.classList.remove('footer-sticky');
+            }
+            else if (isFooterVisible(footer) && !button.classList.contains('footer-sticky')) {
+                button.classList.add('footer-sticky');
+            }
+        }
+        // If there's a sidenav, we might want to show the button anyway
         else {
-            let rect = sidenav.getBoundingClientRect();
-            if (rect.bottom < 0 && button.classList.contains('d-none')) {
-                button.classList.remove('d-none');
-                let eventShow = new Event('fds.backtotop.displayed');
-                button.dispatchEvent(eventShow);
+            let sidenav = document.querySelector('.sidenav-list'); // Finds side navigations (left menus) and step guides
+
+            if (sidenav && sidenav.offsetParent !== null) {
+                // Only react to sidenavs, which are always visible (i.e. not opened from overflow-menu buttons)
+                if (!(sidenav.closest(".overflow-menu-inner")?.previousElementSibling?.getAttribute('aria-expanded') === "true" &&
+                sidenav.closest(".overflow-menu-inner")?.previousElementSibling?.offsetParent !== null)) {
+                    
+                    let rect = sidenav.getBoundingClientRect();
+                    if (rect.bottom < 0) {
+                        if (!isFooterVisible(footer) && button.classList.contains('footer-sticky')) {
+                            button.classList.remove('footer-sticky');
+                        }
+                        else if (isFooterVisible(footer) && !button.classList.contains('footer-sticky')) {
+                            button.classList.add('footer-sticky');
+                        }
+                    }
+                    else {
+                        if (!button.classList.contains('footer-sticky')) {
+                            button.classList.add('footer-sticky');
+                        }
+                    }
+
+                }
             }
-            else if (rect.bottom >= 0 && !button.classList.contains('d-none')) {
-                button.classList.add('d-none');
-                let eventHide = new Event('fds.backtotop.hidden');
-                button.dispatchEvent(eventHide);
+            // There's no sidenav and we know the user hasn't reached the scroll limit: Ensure the button is hidden
+            else {
+                if (!button.classList.contains('footer-sticky')) {
+                    button.classList.add('footer-sticky');
+                }
             }
+        }
+    }
+
+}
+
+function isFooterVisible(footerElement) {
+    if (footerElement?.querySelector('.footer')) {
+        let rect = footerElement.querySelector('.footer').getBoundingClientRect();
+
+        // Footer is visible or partly visible
+        if ((rect.top < window.innerHeight || rect.top < document.documentElement.clientHeight)) {
+            return true;
+        }
+        // Footer is hidden
+        else {
+            return false;
         }
     }
     else {
-        sidenavPresent = false;
-    }
-
-    // If there's no sidenav, use the limit to decide whether to show or hide the back-to-top button
-    if (!sidenavPresent) {
-        if (lastKnownScrollPosition >= limit && button.classList.contains('d-none')) {
-            button.classList.remove('d-none');
-            let eventShow = new Event('fds.backtotop.displayed');
-            button.dispatchEvent(eventShow);
-        }
-        else if (lastKnownScrollPosition < limit && !button.classList.contains('d-none')) {
-            button.classList.add('d-none');
-            let eventHide = new Event('fds.backtotop.hidden');
-            button.dispatchEvent(eventHide);
-        }
+        return false;
     }
 }
 
