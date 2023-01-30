7856b65f5ec893d64b6b005e32892cd286728536
Helle Juhl <hef@netcompany.com>
2023-01-30 15:17:12 +0100

    Removed screen reader messages from table javascript

--- src/js/components/selectable-table.js
+++ src/js/components/selectable-table.js
@@ -1,19 +1,11 @@
 'use strict';
-let text = {
-  "select_row": "Vælg række",
-  "unselect_row": "Fravælg række",
-  "select_all_rows": "Vælg alle rækker",
-  "unselect_all_rows": "Fravælg alle rækker"
-}
 
 /**
  * 
  * @param {HTMLTableElement} table Table Element
- * @param {JSON} strings Translate labels: {"select_row": "Vælg række", "unselect_row": "Fravælg række", "select_all_rows": "Vælg alle rækker", "unselect_all_rows": "Fravælg alle rækker"}
  */
-function TableSelectableRows (table, strings = text) {
+function TableSelectableRows (table) {
   this.table = table;
-  text = strings;
 }
 
 /**
@@ -69,18 +61,14 @@ function updateCheckboxList(e){
     for(let c = 0; c < checkboxList.length; c++){
       checkboxList[c].checked = true;
       checkboxList[c].parentNode.parentNode.classList.add('table-row-selected');
-      checkboxList[c].nextElementSibling.setAttribute('aria-label', text.unselect_row);
     }
 
     checkedNumber = checkboxList.length;
-    checkbox.nextElementSibling.setAttribute('aria-label', text.unselect_all_rows);
   } else{
     for(let c = 0; c < checkboxList.length; c++){
       checkboxList[c].checked = false;
       checkboxList[c].parentNode.parentNode.classList.remove('table-row-selected');
-      checkboxList[c].nextElementSibling.setAttribute('aria-label', text.select_row);
     }
-    checkbox.nextElementSibling.setAttribute('aria-label', text.select_all_rows);
   }
   
   const event = new CustomEvent("fds.table.selectable.updated", {
@@ -99,10 +87,8 @@ function updateGroupCheck(e){
   // update label for event checkbox
   if(e.target.checked){
     e.target.parentNode.parentNode.classList.add('table-row-selected');
-    e.target.nextElementSibling.setAttribute('aria-label', text.unselect_row);
   } else{
     e.target.parentNode.parentNode.classList.remove('table-row-selected');
-    e.target.nextElementSibling.setAttribute('aria-label', text.select_row);
   }
   let table = e.target.parentNode.parentNode.parentNode.parentNode;
   let tableSelectableRows = new TableSelectableRows(table);
@@ -122,15 +108,12 @@ function updateGroupCheck(e){
     if(checkedNumber === checkboxList.length){ // if all rows has been selected
       groupCheckbox.removeAttribute('aria-checked');
       groupCheckbox.checked = true;
-      groupCheckbox.nextElementSibling.setAttribute('aria-label', text.unselect_all_rows);
     } else if(checkedNumber == 0){ // if no rows has been selected
       groupCheckbox.removeAttribute('aria-checked');
       groupCheckbox.checked = false;
-      groupCheckbox.nextElementSibling.setAttribute('aria-label', text.select_all_rows);
     } else{ // if some but not all rows has been selected
       groupCheckbox.setAttribute('aria-checked', 'mixed');
       groupCheckbox.checked = false;
-      groupCheckbox.nextElementSibling.setAttribute('aria-label', text.select_all_rows);
     }
     const event = new CustomEvent("fds.table.selectable.updated", {
       bubbles: true,
