adb1d6a7339df9ce2f58e83ca05ef3c4cc704241
Helle Juhl <hef@netcompany.com>
2023-02-01 20:38:14 +0100

    Added scope to table headers

--- src/js/components/table.js
+++ src/js/components/table.js
@@ -18,7 +18,10 @@ function insertHeaderAsAttributes(tableEl) {
 
     let header = tableEl.getElementsByTagName('thead');
     if (header.length !== 0) {
-        let headerCellEls = header[0].querySelectorAll("th, td");
+        let headerCellEls = header[0].getElementsByTagName('th');
+        if (headerCellEls.length == 0) {
+            headerCellEls = header[0].getElementsByTagName('td');
+        }
 
         if (headerCellEls.length > 0) {
             const bodyRowEls = select('tbody tr', tableEl);
@@ -27,7 +30,7 @@ function insertHeaderAsAttributes(tableEl) {
                 if (cellEls.length === headerCellEls.length) {
                     Array.from(headerCellEls).forEach((headerCellEl, i) => {
                         // Grab header cell text and use it body cell data title.
-                        if (!cellEls[i].hasAttribute('data-title') && headerCellEl.tagName === "TH") {
+                        if (!cellEls[i].hasAttribute('data-title') && headerCellEl.tagName === "TH" && !headerCellEl.classList.contains("actions-header")) {
                             cellEls[i].setAttribute('data-title', headerCellEl.textContent);
                         }
                     });
